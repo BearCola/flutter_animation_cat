97e58270010cbe92f49f291ccb7cb921b41fa4fd
Sangin Kang <sangin84@gmail.com>
2019-09-12 06:22:39 +0900

    Finish project

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -19,14 +19,14 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
     super.initState();
 
     boxController =
-        AnimationController(duration: Duration(seconds: 2), vsync: this);
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
     boxAnimation = Tween(
       begin: pi * 0.6,
       end: pi * 0.65,
     ).animate(
       CurvedAnimation(
         parent: boxController,
-        curve: Curves.linear,
+        curve: Curves.easeInOut,
       ),
     );
     boxAnimation.addStatusListener((status) {
@@ -64,6 +64,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
               buildCatAnimation(),
               buildBox(),
               buildLeftFlap(),
+              buildRightFlap(),
             ],
           ),
         ),
@@ -129,7 +130,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
         builder: (context, child) {
           return Transform.rotate(
             child: child,
-            angle: boxAnimation.value,
+            angle: -boxAnimation.value,
             alignment: Alignment.topRight,
           );
         },
@@ -139,8 +140,10 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
 
   onTap() {
     if (catController.status == AnimationStatus.completed) {
+      boxController.forward();
       catController.reverse();
     } else if (catController.status == AnimationStatus.dismissed) {
+      boxController.stop();
       catController.forward();
     }
   }
